5c59c19fee73588f3c03bc93dc5fc3e3600edbaa
MolchanovSergii <molchanse@gmail.com>
2023-07-16 17:17:39 +0300

    start

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.to startr</Text>
+      <Text>Open up App.to startrr</Text>
       <StatusBar style="auto" />
     </View>
   );
